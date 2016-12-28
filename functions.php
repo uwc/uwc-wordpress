3107ab468e3dab370dda1253c2c6f6c78c0fade4
Connor Bär <hello@connorbaer.io>
2016-12-28 22:56:20 +0100

    Bumps version to 2.1.3.
    Fixes cut off functions file.

--- functions.php
+++ functions.php
@@ -14,105 +14,10 @@ if ( version_compare( $GLOBALS['wp_version'], '4.4-alpha', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
 }
 
-if ( ! function_exists( 'uwc_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function uwc_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 */
-		load_theme_textdomain( 'uwc', trailingslashit( get_template_directory() ) . 'languages' );
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-		set_post_thumbnail_size( 1440 );
-
-		// Enable support for custom header used for logo.
-		add_theme_support( 'custom-header', array(
-			'default-image'          => get_template_directory_uri() . '/images/header-uwc.png',
-			'width'                  => 210,
-			'height'                 => 32,
-			'flex-width'             => true,
-			'uploads'                => true,
-		) );
-
-		register_default_headers( array(
-			'uwc' => array(
-				'url'           => '%s/images/header-uwc.png',
-				'thumbnail_url' => '%s/images/header-uwc-thumbnail.png',
-				'description'   => __( 'UWC', 'uwc' ),
-			),
-			'uwcde' => array(
-				'url'           => '%s/images/header-uwcde.png',
-				'thumbnail_url' => '%s/images/header-uwcde-thumbnail.png',
-				'description'   => __( 'UWC Germany', 'uwc' ),
-			),
-		) );
-
-		// This theme uses wp_nav_menu() in two locations.
-		register_nav_menus( array(
-			'primary' => __( 'Primary Menu', 'uwc' ),
-			'social' => __( 'Social Links Menu', 'uwc' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
-
-		/*
-		 * Enable support for Post Formats.
-		 *
-		 * See: https://codex.wordpress.org/Post_Formats
-		 */
-		add_theme_support( 'post-formats', array(
-			'video',
-			'link',
-			'gallery',
-		) );
-
-		/*
-		 * Add support for Post Formats and Excerpts to pages.
-		 *
-		 * See: https://codex.wordpress.org/Function_Reference/add_post_type_support
-		 */
-		add_post_type_support( 'page', array( 'post-formats', 'excerpt' ) );
-
-		/*
-		 * This theme styles the visual editor to resemble the theme style,
-		 * specifically font, colors, icons, and column width.
-		 */
-		add_editor_style( array( 'editor-style.css', uwc_fonts_url() ) );
-	}
-endif;
-add_action( 'after_setup_theme', 'uwc_setup' );
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ */
+require get_template_directory() . '/inc/theme.php';
 
 /**
  * Sets the maximum media width for embed media in pixels, based on the theme's design and stylesheet.
@@ -248,4 +153,16 @@ require get_template_directory() . '/inc/template-tags.php';
 /**
  * Custom functions that act independently of the theme templates.
  */
-require
+require get_template_directory() . '/inc/extras.php';
+/**
+ * Custom widgets.
+ */
+require get_template_directory() . '/inc/widgets.php';
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
